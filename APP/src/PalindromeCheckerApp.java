603ca587b269be3a09ad0b27c73380f61b983dd4
sathvikchetty18 <sathvikchetty18@gmail.com>
2026-02-27 15:42:48 +0530

    UC2 done

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
     public static void main (String[] args){
-        System.out.println("Welcome to Palindrome Checker App Management System");
+
+        Scanner sc =new Scanner(System.in);
+
+        int num=sc.nextInt();
+        int original =num;
+        int reverse=0;
+        while(num>0){
+            int lastDigit=num%10;
+            reverse=(reverse*10)+lastDigit;
+            num/=10;
+        }
+        if(original==reverse)
+            System.out.println(original + "is a palindrome");
+            else
+            System.out.println(original+"is a not palindrome");
+
+
+            sc.close();
+
     }
 }
